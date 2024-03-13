d214cbaff258b89b7f9eb2a1e857adf1fc373eff
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-03-13 11:57:47 +0800

    bug fix

--- app/src/main/java/com/example/androidprojectcollection/CalculatorExercise.java
+++ app/src/main/java/com/example/androidprojectcollection/CalculatorExercise.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.function.DoubleUnaryOperator;
+
 public class CalculatorExercise extends AppCompatActivity {
 
     Button calc0;
@@ -24,9 +28,18 @@ public class CalculatorExercise extends AppCompatActivity {
     Button calcPlus;
     Button calcDot;
     Button calcEquals;
+    Button calcDelete;
 
     TextView typedOutput;
     TextView finalOutput;
+
+    StringBuilder typedText;
+    StringBuilder finalText;
+
+    Queue<Double> seqQueueNum = new LinkedList();
+    Queue<Character> seqQueueOp = new LinkedList();
+
+    boolean decimaled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,72 +62,197 @@ public class CalculatorExercise extends AppCompatActivity {
         calcMinus = findViewById(R.id.calcMinus);
         calcDot = findViewById(R.id.calcDot);
         calcEquals = findViewById(R.id.calcEquals);
+        calcDelete = findViewById(R.id.calcDelete);
 
         typedOutput = findViewById(R.id.typedOutput);
         finalOutput = findViewById(R.id.factoredOutput);
 
-        calc0.setOnClickListener(view -> {
+        typedOutput.setText("0");
+        finalOutput.setText("0");
 
+        typedText = new StringBuilder();
+        finalText = new StringBuilder();
+
+        calc0.setOnClickListener(view -> {
+            typeNum('0');
         });
 
         calc1.setOnClickListener(view -> {
-
+           typeNum('1');
         });
 
         calc2.setOnClickListener(view -> {
-
+            typeNum('2');
         });
 
         calc3.setOnClickListener(view -> {
-
+            typeNum('3');
         });
 
         calc4.setOnClickListener(view -> {
-
+            typeNum('4');
         });
 
         calc5.setOnClickListener(view -> {
-
+            typeNum('5');
         });
 
         calc6.setOnClickListener(view -> {
-
+            typeNum('6');
         });
 
         calc7.setOnClickListener(view -> {
-
+            typeNum('7');
         });
 
         calc8.setOnClickListener(view -> {
-
+            typeNum('8');
         });
 
         calc9.setOnClickListener(view -> {
-
+            typeNum('9');
         });
 
         calcDivide.setOnClickListener(view -> {
-
+            typeOperation('/');
         });
 
         calcMultiply.setOnClickListener(view -> {
-
+            typeOperation('*');
         });
 
         calcPlus.setOnClickListener(view -> {
-
+            typeOperation('+');
         });
 
         calcMinus.setOnClickListener(view -> {
-
+            typeOperation('-');
         });
 
+        //Add it so its allowed on a lone number
         calcDot.setOnClickListener(view -> {
-
+            if(typedText.length() == 0){
+                typedText.append("0.");
+            }else{
+                if(typedLast() == '.'){
+                    typedText.deleteCharAt(typedText.length()-1);
+                    decimaled = false;
+                }else if(!decimaled){
+                    typedText.append(".");
+                    decimaled = true;
+                }
+            }
+            typedOutput.setText(typedText);
         });
 
         calcEquals.setOnClickListener(view -> {
 
         });
+
+        calcDelete.setOnClickListener(view -> {
+            if(typedText.length() > 0){
+                if(typedLast() == '.'){
+                    decimaled = false;
+                }
+                typedText.deleteCharAt(typedText.length()-1);
+            }
+            typedOutput.setText(typedText);
+            evaluateSeqExpression();
+        });
+    }
+
+    void typeNum(char num){
+        if(typedText.length() == 0){
+            typedText.append(num);
+        } else if(typedText.length() == 1 && typedLast() == '0'){
+            typedText.setCharAt(typedText.length()-1, num);
+        }else{
+            typedText.append(num);
+        }
+        typedOutput.setText(typedText);
+        evaluateSeqExpression();
+    }
+
+    void typeOperation(char op){
+        if(typedText.length() == 0){
+            return;
+        }else if(typedLast() == '+' || typedLast() == '-' || typedLast() == '/' || typedLast() == '*'){
+            typedText.setCharAt(typedText.length()-1, op);
+        }else{
+            typedText.append(op);
+            decimaled = false;
+        }
+        typedOutput.setText(typedText);
+        evaluateSeqExpression();
+    }
+
+    void evaluateSeqExpression(){
+        if(typedText.length() <= 0){
+            seqQueueNum.clear();
+            return;
+        }
+
+        if(typedLast() == '+' || typedLast() == '-' || typedLast() == '/' || typedLast() == '*'){
+            return;
+        }
+
+        double seqNum = 0;
+        double temp = 0;
+
+        int decimalState = 0;
+
+        //bug: double operation ma queue
+
+        for(int i = 0; i < typedText.length(); i++){
+            if(Character.isDigit(typedText.charAt(i))){
+                if(decimalState > 0){
+                    seqNum += Character.digit(typedText.charAt(i), 10) / (Math.pow(10,decimalState));
+                    decimalState++;
+                }else if(seqNum != 0){
+                    seqNum *= 10;
+                    seqNum += Character.digit(typedText.charAt(i), 10);
+                }else{
+                    seqNum = Character.digit(typedText.charAt(i), 10);
+                }
+
+                if(i+1 >= typedText.length()){
+                    seqQueueNum.add(seqNum);
+                }
+            } else if(typedText.charAt(i) == '.'){
+                decimalState = 1;
+            } else{
+                decimalState = 0;
+                seqQueueNum.add(seqNum);
+                seqQueueOp.add(typedText.charAt(i));
+                seqNum = 0;
+            }
+        }
+
+        char opState;
+
+//        while(!seqQueueNum.isEmpty()){
+//            if(seqQueueOp.isEmpty())
+//                break;
+//            seqNum = seqQueueNum.remove();
+//            opState = seqQueueOp.remove();
+//
+//            switch(opState){
+//                case '+':
+//                    seqNum += seqQueueNum.remove();
+//                case '-':
+//                    seqNum -= seqQueueNum.remove();
+//                case '*':
+//                    seqNum *= seqQueueNum.remove();
+//                case '/':
+//                    seqNum /= seqQueueNum.remove();
+//            }
+//        }
+
+        finalOutput.setText(seqQueueNum.toString() + " - " + seqQueueOp.toString());
+        seqQueueNum.clear();
+    }
+
+    char typedLast(){
+        return typedText.charAt(typedText.length()-1);
     }
 }
