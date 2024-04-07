1d3dcd8981dccaaf1ca0fb06ee74e917ccae8e3c
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-04-07 15:03:53 +0800

    ngano man ka maurice

--- app/src/main/java/com/example/androidprojectcollection/Connect3Exercise.java
+++ app/src/main/java/com/example/androidprojectcollection/Connect3Exercise.java
@@ -10,10 +10,12 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class Connect3Exercise extends AppCompatActivity {
 
     Button[][] allButtons;
-    Boolean[][] allStatus;
+    Integer[][] allStatus;
 
     Boolean currentPlayer = false;
 
@@ -27,7 +29,7 @@ public class Connect3Exercise extends AppCompatActivity {
         setContentView(R.layout.activity_connect3_exercise);
 
         allButtons = new Button[5][5];
-        allStatus = new Boolean[5][5];
+        allStatus = new Integer[5][5];
 
         currentPlayerText = findViewById(R.id.currentPlayer);
         currentPlayerText.setText("Player 1's Turn:");
@@ -38,32 +40,27 @@ public class Connect3Exercise extends AppCompatActivity {
             for (int col = 0; col < 5; col++){
                 String resIDName = "connect" + row + col;
                 allButtons[row][col] = findViewById(this.getResources().getIdentifier(resIDName, "id", this.getPackageName()));
-                allStatus[row][col] = false;
+                allStatus[row][col] = 0;
             }
         }
 
         allButtons[0][0].setOnClickListener(view -> {
-            System.out.println("in the real way");
             dropCoin(0);
         });
 
         allButtons[0][1].setOnClickListener(view -> {
-            System.out.println("in the real way");
             dropCoin(1);
         });
 
         allButtons[0][2].setOnClickListener(view -> {
-            System.out.println("in the real way");
             dropCoin(2);
         });
 
         allButtons[0][3].setOnClickListener(view -> {
-            System.out.println("in the real way");
             dropCoin(3);
         });
 
         allButtons[0][4].setOnClickListener(view -> {
-            System.out.println("in the real way");
             dropCoin(4);
         });
 
@@ -71,8 +68,8 @@ public class Connect3Exercise extends AppCompatActivity {
             for(int row = 0; row < 5; row++){
                 for (int col = 0; col < 5; col++){
                     allButtons[row][col].setBackgroundTintList(getColorStateList(R.color.white));
-                    allStatus[row][col] = false;
-                    currentPlayer = true;
+                    allStatus[row][col] = 0;
+                    currentPlayer = false;
                     currentPlayerText.setText("Player 1's Turn:");
                 }
             }
@@ -80,25 +77,149 @@ public class Connect3Exercise extends AppCompatActivity {
     }
 
     void dropCoin(int position){
-        for(int row = 1; row <= 5; row++){
-            if(row < 5 && !allStatus[row][position]){
+        for(int row = 0; row <= 5; row++){
+            if(row == 0 && allStatus[row][position] > 0){
+                return;
+            }
+
+            if(row < 5 && allStatus[row][position] == 0){
                 continue;
             }
-            allStatus[row-1][position] = true;
+            allStatus[row-1][position] = currPlayerCoin();
             allButtons[row-1][position].setBackgroundTintList(currCoinColor());
+
+            System.out.println("------------------------------------------------------------");
+            for(int i = 0; i < 5; i++){
+                System.out.println(Arrays.toString(allStatus[i]));
+            }
+            System.out.println("------------------------------------------------------------");
+
+//            System.out.println("STATUS DIAGONAL BACK: " + checkWinDiagonalBack(currPlayerCoin(), row-1 , position));
+//            System.out.println("STATUS DIAGONAL FRONT: " + checkWinDiagonalFront(currPlayerCoin(), row-1 , position));
+//            System.out.println("STATUS HORIZONTAL: " + checkWinHorizontal(currPlayerCoin(), row-1 , position));
+//            System.out.println("STATUS VERTICAL: " + checkWinVertical(currPlayerCoin(), row-1 , position));
+            if(checkWin(currPlayerCoin(), row-1, position)){
+                currentPlayerText.setText("Somebody won!");
+                return;
+            }
+
+            currentPlayer ^= true;
             break;
         }
     }
 
     ColorStateList currCoinColor(){
-        currentPlayer ^= true;
         if(currentPlayer){
-            currentPlayerText.setText("Player 1's Turn:");
+            currentPlayerText.setText("Player 2's Turn:");
             return getColorStateList(R.color.green);
         }else{
-            currentPlayerText.setText("Player 2's Turn:");
+            currentPlayerText.setText("Player 1's Turn:");
             return getColorStateList(R.color.orange);
         }
+    }
+
+    int currPlayerCoin(){
+        if(currentPlayer){
+            return 2;
+        }else{
+            return 1;
+        }
+    }
+
+    boolean checkWin(int player, int row, int col){
+        if(checkWinHorizontal(player, row, col) >= 3){
+            return true;
+        }
+        if(checkWinVertical(player, row, col) >= 3){
+            return true;
+        }
+        if(checkWinDiagonalFront(player, row, col) >= 3){
+            return true;
+        }
+        if(checkWinDiagonalBack(player, row, col) >= 3){
+            return true;
+        }
+        return false;
+    }
+
+    //DIAGONAL BACK
+    int checkWinDiagonalBack(int player, int row, int col){
+        //MARIE STOPPPP
+        return checkWinDiagonalBackLeft(player, row+1, col-1) +  checkWinDiagonalBackRight(player, row-1, col+1) + 1;
+    }
+    int checkWinDiagonalBackLeft(int player, int row, int col){
+        //MARIE STOPPPP
+        if(row >= 5 || row < 0 || col >= 5 || col < 0 || player != allStatus[row][col]){
+            return 0;
+        }
+        return checkWinDiagonalBackLeft(player, row+1, col+1) + 1;
+    }
+    int checkWinDiagonalBackRight(int player, int row, int col){
+        //MARIE STOPPPP
+        if(row >= 5 || row < 0 || col >= 5 || col < 0 || player != allStatus[row][col]){
+            return 0;
+        }
+        return checkWinDiagonalBackRight(player, row-1, col-1) + 1;
+    }
+
+    //DIAGONAL FRONT
+    int checkWinDiagonalFront(int player, int row, int col){
+        //MARIE STOPPPP
+        return checkWinDiagonalFrontLeft(player, row+1, col+1) +  checkWinDiagonalFrontRight(player, row-1, col-1) + 1;
+    }
+    int checkWinDiagonalFrontLeft(int player, int row, int col){
+        //MARIE STOPPPP
+        if(row >= 5 || row < 0 || col >= 5 || col < 0 || player != allStatus[row][col]){
+            return 0;
+        }
+        return checkWinDiagonalFrontLeft(player, row+1, col+1) + 1;
+    }
+    int checkWinDiagonalFrontRight(int player, int row, int col){
+        //MARIE STOPPPP
+        if(row >= 5 || row < 0 || col >= 5 || col < 0 || player != allStatus[row][col]){
+            return 0;
+        }
+        return checkWinDiagonalFrontRight(player, row-1, col-1) + 1;
+    }
+
+    //HORIZONTAL
+    int checkWinHorizontal(int player, int row, int col){
+        //MARIE STOPPPP
+        return checkWinHorizontalLeft(player, row, col-1) +  checkWinHorizontalRight(player, row, col+1) + 1;
+    }
+    int checkWinHorizontalLeft(int player, int row, int col){
+        //MARIE STOPPPP
+        if(col >= 5 || col < 0 || player != allStatus[row][col]){
+            return 0;
+        }
+        return checkWinHorizontalLeft(player, row, col-1) + 1;
+    }
+    int checkWinHorizontalRight(int player, int row, int col){
+        //MARIE STOPPPP
+        if(col >= 5 || col < 0 || player != allStatus[row][col]){
+            return 0;
+        }
+        return checkWinHorizontalRight(player, row-1, col+1) + 1;
+    }
+
+    //VERTICAL CHECKER
+    int checkWinVertical(int player, int row, int col){
+        //MARIE STOPPPP
+        return checkWinVerticalUp(player, row+1, col) +  checkWinVerticalDown(player, row-1, col) + 1;
+    }
+    int checkWinVerticalUp(int player, int row, int col){
+        //MARIE STOPPPP
+        if(row >= 5 || row < 0 || player != allStatus[row][col]){
+            return 0;
+        }
+        return checkWinVerticalUp(player, row+1, col) + 1;
+    }
+    int checkWinVerticalDown(int player, int row, int col){
+        //MARIE STOPPPP
+        if(row >= 5 || row < 0 || player != allStatus[row][col]){
+            return 0;
+        }
+        return checkWinVerticalDown(player, row-1, col) + 1;
     }
 
 
